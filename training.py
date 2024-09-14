fdff423294f72cf65bb8955b43f508df19077c68
BarbieMunoz <a01659348@tec.mx>
2024-09-14 14:56:47 -0600

    trains the thingyy to give you the best answer

--- training.py
+++ training.py
@@ -35,4 +35,62 @@ for intent in intents['intents']:
 words= [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words=sorted(set(words))
 
-print(words)
+#print(words)
+
+
+classes = sorted(set(classes))
+
+# save the words and classes in a pickle file
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    # obtain the word patterns for each document
+    word_patterns = document[0]
+    # lemmatize each word in the word patterns
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
+    # create the bag of words depending on whether the word is in the word patterns
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    # create the output row with the same lenght of the number of classes
+    output_row = list(output_empty)
+    #marks with 1 the position of the class in the output row
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+# shuffle the training data
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+# create the train_x and train_y to divide the data of the bag and output row
+#train_x = list(training[:, 0])
+#train_y = list(training[:, 1])
+
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(len(train_x[0]),)))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+initial_learning_rate = 0.01
+decay_steps = 100000
+decay_rate = 1e-6
+learning_rate_schedule = keras.optimizers.schedules.InverseTimeDecay(initial_learning_rate, decay_steps, decay_rate)
+
+sgd = SGD(learning_rate=learning_rate_schedule, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+model.save('chatbot_model.keras')
+print('Done')
