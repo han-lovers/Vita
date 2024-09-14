551d71109b564fd97670be88975d6b46166fafc1
leopalatto20 <A01665462@tec.mx>
2024-09-14 15:38:12 -0600

    Crear archivos y agregar pagina principal

---
+++ my_vita_app/static/js/home.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Podrías añadir alguna animación o interacción al botón de Sign Up aquí
+    const signupBtn = document.querySelector('.btn-signup');
+    signupBtn.addEventListener('click', function() {
+        alert('¡Gracias por unirte a Banorte!');
+    });
+});
