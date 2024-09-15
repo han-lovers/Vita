03938e90e619a2f52f0dd2116fc384e739407100
leopalatto20 <A01665462@tec.mx>
2024-09-14 20:34:21 -0600

    Todo agregado y reactivo

--- my_vita_app/views.py
+++ my_vita_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,7 @@ def home(request):
 
 def signup(request):
     return render(request, 'signup.html')
+def faceUpload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES.get('photo')
+    return render(request, 'faceUpload.html')
--- vita/urls.py
+++ vita/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from my_vita_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('templates/signup/', views.signup, name='signup'),
+    path('templates/faceUpload/', views.faceUpload, name='faceUpload'),
 ]
